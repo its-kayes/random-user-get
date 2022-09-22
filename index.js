10a59785aa56fbd44d7417b37bae8f8162471729
its-kayes <kayes.ek8@gmail.com>
2022-09-23 01:59:02 +0600

    Setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+const router = express.Router();
+const file = require('file-system');
+const fs = require('fs');
+const readFile = require('./middleware/readFile.middleware');
+
+
+
+
+
+app.get("/all",readFile, (req, res) => {
+    // let data = fs.writeFile('users.json', 'aaa', function(err) { console.log(err) });
+    // fs.readFile('users.json', function (err, data) {
+    //     res.send({ result: data, success: true });
+    // })
+
+    console.log(req);
+    res.send("got");
+
+    // fs.readFile('users.json', function(err, data) {
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     // const resu = fs.write(data);
+    //     // console.log(resu);
+    //     res.write(data);
+    //     return res.end();
+    //   });
+})
+
+
+// check get route 
+app.get('/', (req, res) => {
+    res.send('Random User Load');
+})
+
+
+// Port Connection 
+app.listen(port, () => {
+    console.log(`app running on port ${port}`);
+})
