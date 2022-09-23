adb3663ecf753949c86822c749228da2eee87264
its-kayes <kayes.ek8@gmail.com>
2022-09-23 09:38:55 +0600

    get all user , get random id

--- controller/UserRouteController.js
+++ controller/UserRouteController.js
@@ -1,6 +1,29 @@
 const users = require("../users.json");
 
 module.exports.getAllUsers = async (req, res) => {
-    console.log(users);
-    res.send(users);
+    try {
+        const query = req.query;
+        if (query) {
+            const { limit } = query;
+            const data = await users.slice(0, limit);
+            res.send(data)
+        }
+        else {
+            res.send(users)
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error.massage);
+    }
+}
+
+module.exports.randomUser = async (req, res) => {
+    try {
+        const data = users;
+        const randomId = Math.floor(Math.random() * data.length);
+        console.log(randomId);
+        res.send({id: randomId});
+    } catch (error) {
+
+    }
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersRoute = require("../controller/UserRouteController.js");
 
 router
-    .get("/all", usersRoute.getAllUsers);
-
+    .get("/all", usersRoute.getAllUsers)
+    .get("/random", usersRoute.randomUser)
 
 module.exports = router;
